11ec19263a32dda15efbbe49dc453204ff327c44
Chillintano <chilintano1340@yahoo.com>
2015-03-15 19:31:45 +0300

    Модель ActiveRaffle

---
+++ app/models/active_raffle.rb
@@ -0,0 +1,8 @@
+class ActiveRaffle < ActiveRecord::Base
+  validates :item_pool_id, presence: true
+  validates :icon_url, presence: true
+  validates :item_name, presence: true
+  validates :item_value, presence: true
+  validates :num_slots, presence: true
+  validates :slot_value, presence: true
+end
