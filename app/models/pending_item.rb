8d7d744117a438b0c2bdb439741bbadbe3fec933
Chillintano <chilintano1340@yahoo.com>
2015-04-05 16:29:49 +0300

    Model

--- app/models/pending_item.rb
+++ app/models/pending_item.rb
@@ -1,4 +1,5 @@
 class PendingItem < ActiveRecord::Base
+  belongs_to :price
   validates :item_steam_id, presence: true
   validates :price_id, presence: true
   validates :bot_id, presence: true
--- app/models/price.rb
+++ app/models/price.rb
@@ -2,6 +2,7 @@ class Price < ActiveRecord::Base
   has_many :items
   has_many :short_finished_raffles
   has_many :stats
+  has_many :pending_items
   validates :item_hash_name, presence: true
   validates :item_cost, presence: true
   validates :last_update, presence: true
